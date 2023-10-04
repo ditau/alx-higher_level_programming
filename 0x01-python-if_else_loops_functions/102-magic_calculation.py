d75177c8a9beafc6aa88580ea785c457457d43c4
ditau <ditaumolatedi@gmail.com>
2023-10-03 17:57:18 -0700

    fx

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,9 @@
-c_calculation.py
-
+#!/usr/bin/python3
 
 def magic_calculation(a, b, c):
-        """Match bytecode provided by Holberton School."""
-            if a < b:
-                        return (c)
-                        if c > b:
-                                    return (a + b)
-                                    return (a*b - c)
+
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
