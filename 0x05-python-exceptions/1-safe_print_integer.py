c4669e38c472316da9a05d236335522d73cffe29
ditau <ditaumolatedi@gmail.com>
2023-10-23 15:31:23 -0700

    fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 
-
-
 def safe_print_integer(value):
-
     try:
-
         print("{:d}".format(value))
-
-        return (True)
-
+        return True
     except (TypeError, ValueError):
-
-        return (False)
+        return False
