d0922c855b0e4d50256610082eb26287da42a82a
ditau <ditaumolatedi@gmail.com>
2023-10-24 17:26:38 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/bin/user/python3
+"""Square Module"""
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self,size):
+        """Constructor.
+        
+        Args:
+        size: length of a side of a square.
+        """
+        self.__size = size
