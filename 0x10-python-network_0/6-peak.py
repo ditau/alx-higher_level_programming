0dfd1d5b68eb1235025a5c12e9c4769c0110bfc0
ditau <ditaumolatedi@gmail.com>
2024-01-25 14:58:23 -0800

    fix pynet

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,22 +7,18 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or list_of_integers == []:
         return None
-
     lo = 0
-    hi = len(list_of_integers) - 1
-
-    while lo < hi:
-        mid = ((hi - lo) // 2) + lo
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            lo = mid + 1
-        else:
-            hi = mid
-
-    peak = lo  # Corrected to store the potential peak index
-
-    # Check if the potential peak is actually a peak
-    if (peak == 0 or list_of_integers[peak] >= list_of_integers[peak - 1]) and \
-       (peak == len(list_of_integers) - 1 or list_of_integers[peak] >= list_of_integers[peak + 1]):
-        return list_of_integers[peak]
-    else:
-        return None
+    hi = len(list_of_integers)
+    mid = ((hi + lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
